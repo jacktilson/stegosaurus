435f6e7f5c0f05e3ccf910b0faf8baadf165aa14
Jack Tilson <jacktilson80@gmail.com>
2019-07-22 17:28:19 +0000

    Tidy up steg_server code files

--- steg_server/error_routes.py
+++ steg_server/error_routes.py
@@ -1,8 +1,16 @@
 """ This file contains the routes for all custom error handling within Flask """
 
+#####################
+# Load Dependencies #
+#####################
+
 from app import app
 from flask import render_template
 from error_code_info import error_info
+
+################################
+# HTTP Exception Handler Route #
+################################
 
 @app.errorhandler(Exception)
 def http_error(error):
@@ -12,20 +20,17 @@ def http_error(error):
     
     # Acquire an explanation of the error code from error_code_info file, less the code itself.
     if error.code in error_info:
-    
       # If found, set specific description.
       err_desc = error_info[error.code][4:]
-      
       # Declare whether we know about the error.
       err_code_known = True
     
     else:
-      
       # If it is not found, set a generic one.
       err_desc = gen_err_desc
-      
       # Declare whether we know about the error.
       err_code_known = False
-    
+      
     # Render the error page.
-    return render_template('http_error.html', err_code = error.code, err_desc = err_desc, err_code_known = err_code_known), error.code
+    return render_template('http_error.html', err_code = error.code, err_desc = err_desc,
+                           err_code_known = err_code_known), error.code
