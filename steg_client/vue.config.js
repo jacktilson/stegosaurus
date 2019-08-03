6a6e493b8b08efe93846993ef677cff96897c688
Jack Tilson <jacktilson80@gmail.com>
2019-08-03 08:45:49 +0000

    Alter file paths such that they work with apache

--- steg_client/vue.config.js
+++ steg_client/vue.config.js
@@ -1,5 +1,5 @@
 // vue.config.js
 module.exports = {
-  outputDir: '../steg_server/templates',
-  assetsDir: '../static' //relative to outputDir
-} 
+  outputDir: "../steg_server/templates",
+  assetsDir: "../static" //relative to outputDir
+};
