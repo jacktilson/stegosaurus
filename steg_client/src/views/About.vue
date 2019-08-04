d8d4b51618d26f2baba37e7b075cf4c3900b0aed
Oliver Butler <olly@protoncloud.co.uk>
2019-08-04 15:33:01 +0100

    Start parallax effect

--- steg_client/src/views/About.vue
+++ steg_client/src/views/About.vue
@@ -1,4 +1,9 @@
 <template lang="pug">
-  b-container
-    h1 This is an about page
+b-container
+  b-row
+    b-col(sm="12")
+      b-card.mt-3
+        b-card-title What is Stegosaurus?
+        b-card-text Stegosaurus is a steganography tool designed to allow users to encode data within image files.
+          | We are using a method of steganography known as LSB (Least significant bit) steganography
 </template>
