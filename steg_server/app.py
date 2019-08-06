ece36f81a65dc69108e1e816a64456f5e300060e
Jack Tilson <jacktilson80@gmail.com>
2019-08-06 22:40:00 +0000

    File extension getter for store_file_temp() improved to work with extensions of any length. reply_error_json() now also returning HTTP exception codes with the request response, default is 400 Bad Request for interpretation by vue. POST request sizes limited to 64mb in app.py.

--- steg_server/app.py
+++ steg_server/app.py
@@ -8,13 +8,9 @@ import os
 from flask import Flask
 app = Flask(__name__, static_url_path="/")
 app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'temp')
+app.config['MAX_CONTENT_LENGTH'] = 64 * 1024 * 1024 # Max post request size 64MB
 app.config['CUSTOM_STATIC_PATH'] = os.path.join(app.root_path, 'temp')
 app.config['APP_HOST'] = "stegosaurus.online"
-
-#print(f"APP_ROOT VARIABLE VALUE::::: {app_root}")
-#print(f"app.root_path VARIABLE VALUE::::: {app.root_path}")
-#print(f"app.instance_path VARIABLE VALUE::::: {app.instance_path}")
-#print("*********************************")
 
 #############################
 # Load Core App Page Routes #
