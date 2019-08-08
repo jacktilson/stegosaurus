78b1bd6f04e14dd91498f4628aca2c7e8f8e1d24
Jack Tilson <jacktilson80@gmail.com>
2019-08-08 10:17:36 +0000

    Temp dir no longer accessible via url, file upload test route removed, get_temp_path now replaces get_orig_img_path and the like, variable names more readable, some extra removal of repetitive code.

--- steg_server/core_routes.py
+++ steg_server/core_routes.py
@@ -24,29 +24,6 @@ def home():
       return send_from_directory(os.path.join(app.static_folder, "client"), 'index.html')
     else:
       return send_from_directory(app.static_folder, 'bad_domain.html')
-
-
-##########################
-# File Upload Test Route #
-##########################
-
-@app.route('/filetest', methods = ['POST'])
-def filetest():
-  if request.method == 'POST':
-    f = request.files['file']
-    return send_file(BytesIO(f.read()),
-                    attachment_filename = 'file.bmp')
-  else:
-    return 'no request received'
-  
-  
-#######################################
-# Temp/Upload Files HTTP Access Route #
-#######################################
-
-@app.route('/temp/<path:filename>')
-def temp_access(filename):
-    return send_from_directory(app.config['CUSTOM_STATIC_PATH'], filename, conditional=True)
   
       
 ######################################
@@ -64,8 +41,8 @@ def upload_encode():
   if img_file is None: return reply_error_json('Original image was not present in the POST request.')
   
   # Test if temp folder has capacity for this.
-  temp_dir_cap_test = validate_temp_dir(img_file)
-  if temp_dir_cap_test != True: return temp_dir_cap_test
+  storage_test = validate_temp(img_file)
+  if storage_test != True: return storage_test
   
   # Drop the image in relevant directory, retain path.
   img_abs_path = store_file_temp(trans_id, img_file, 'originals', 'orig')
@@ -99,8 +76,9 @@ def space_encode():
     if n_lsb_test != True: return n_lsb_test
     
     # Perform space analysis.
+    img_path = get_temp_path(trans_id, 'originals', 'orig')[1]
     flags_sa = build_flags(['filename', 'extension'], ['n_lsb'], request)
-    space = space_available(read_img(form_orig_img_path(trans_id)[1]), **flags_sa)
+    space = space_available(read_img(img_path), **flags_sa)
     
     # Hand back a JSON on success.
     return jsonify({"space_available": space})
@@ -129,20 +107,21 @@ def complete_encode():
     if data_file is None: return reply_error_json('The data file was not received.')
 
     # Check whether temp dir can handle this data file.
-    temp_dir_cap_test = validate_temp_dir(data_file)
-    if temp_dir_cap_test != True: return temp_dir_cap_test
+    storage_test = validate_temp(data_file)
+    if storage_test != True: return storage_test
     
-    # Store the data file.
+    # Store the data file and retain its path.
     data_file_path = store_file_temp(trans_id, data_file, 'data', 'data')
     
     # Set the prospective destination file with extension.
-    enc_file_path = form_enc_img_path(trans_id, file_exits=False, file_ext=get_img_ext(trans_id))[1]
+    enc_img_path = get_temp_path(trans_id, 'encoded', 'encoded', file_exists=False, file_ext=get_img_ext(trans_id))[1]
 
     # Read the original image into bytes.
-    in_img = read_img(form_orig_img_path(trans_id)[1])
+    orig_img_path = get_temp_path(trans_id, 'originals', 'orig')[1]
+    in_img = read_img(orig_img_path)
     
     # Read the data file into bytes.
-    in_data = read_data_file_bytes(trans_id)
+    in_data = read_data_bytes(trans_id)
     
     # Process the encoding, feeding original image path, data file path and flags to encode function.
     flags_enc = build_flags(['filename', 'extension'], ['n_lsb'], request)
@@ -151,7 +130,7 @@ def complete_encode():
     encoded_img = encode(in_img, in_data, **flags_enc)
     
     # Store the result
-    write_img(enc_file_path, encoded_img)
+    write_img(enc_img_path, encoded_img)
 
     # Hand off the result.  
     return jsonify({"resp_msg": 'Data encoded to image successfully.'})    
@@ -171,19 +150,16 @@ def download_encode():
     if trans_id_test != True: return trans_id_test
     
     # Obtain the path for the encoded image.
-    enc_img_path_abs = form_enc_img_path(trans_id)[1]
+    img_path = get_temp_path(trans_id, 'encoded', 'encoded')[1]
     
-    # Obtain the filename with extension.
-    enc_img_path_parts = enc_img_path_abs.split(os.sep)
-    enc_img_filename = enc_img_path_parts[len(enc_img_path_parts)-1]
-    # Now obtain the file extension, taking the text after the last dot.
-    enc_img_filename_parts = enc_img_filename.split('.')
-    enc_img_ext = enc_img_filename_parts[len(enc_img_filename_parts)-1]
+    # Obtain the file extension
+    img_ext = get_img_ext(trans_id)
     
-    return_filename = f'output.{enc_img_ext}'
+    # Form the filename of the response image.
+    return_filename = f'output.{img_ext}'
     
     # Send off the encoded file
-    return send_file(enc_img_path_abs, attachment_filename=return_filename, as_attachment=True)
+    return send_file(img_path, attachment_filename=return_filename, as_attachment=True)
  
 ###############################
 # Encode Files Deletion Route #
@@ -199,15 +175,15 @@ def delete_encode():
     trans_id_test = check_trans_id(trans_id, 'encoded')
     if trans_id_test != True: return trans_id_test
     
-    # Obtain the path for the encoded image.
-    orig_img_path_abs = form_orig_img_path(trans_id)[1]
-    data_file_path_abs = form_data_file_path(trans_id)[1]
-    enc_img_path_abs = form_enc_img_path(trans_id)[1]
+    # Obtain the paths for the transaction files.
+    orig_img_path = get_temp_path(trans_id, 'originals', 'orig')[1]
+    data_file_path = get_temp_path(trans_id, 'data', 'data')[1]
+    enc_img_path = get_temp_path(trans_id, 'encoded', 'encoded')[1]
     
     # Cycle though files to see if need to delete. Assuming if not found, can ignore.
-    if orig_img_path_abs is not None: os.remove(orig_img_path_abs)
-    if data_file_path_abs is not None: os.remove(data_file_path_abs)
-    if enc_img_path_abs is not None: os.remove(enc_img_path_abs)
+    if orig_img_path is not None: os.remove(orig_img_path)
+    if data_file_path is not None: os.remove(data_file_path)
+    if enc_img_path is not None: os.remove(enc_img_path)
       
     return jsonify({"resp_msg": 'All encode transaction files deleted successfully.'})
   
