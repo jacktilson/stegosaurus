5b6f1704f79e93b8042b81eb8faf1759d484d213
Oliver Butler <olly@protoncloud.co.uk>
2019-08-08 22:24:43 +0100

    Add img to about

--- steg_client/src/views/About.vue
+++ steg_client/src/views/About.vue
@@ -2,8 +2,11 @@
 b-container
   b-row
     b-col(sm="12")
-      b-card.mt-3
+      b-card.mt-3(img-src="@/assets/about_lsb.svg" img-right)
         b-card-title What is Stegosaurus?
         b-card-text Stegosaurus is a steganography tool designed to allow users to encode data within image files.
-          | We are using a method of steganography known as LSB (Least significant bit) steganography
+          |  We are using a method of steganography known as LSB (Least significant bit) steganography. This involves
+          | using the least significant bits of each channel of each pixel. For example, with a 24 bit, 3 channel image.
+          | each colour channel has 8 bits. eg. 10110000, in this example the least most significant bit is on the far
+          | right side and the decimal of this would be 176. If we were to use 2 LSB for our data file, this would effectively 
 </template>
--- steg_client/vue.config.js
+++ steg_client/vue.config.js
@@ -1,5 +1,26 @@
 // vue.config.js
 module.exports = {
   publicPath: "/static/client",
-  outputDir: "../steg_server/static/client"
+  outputDir: "../steg_server/static/client",
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options['transformAssetUrls'] = {
+          img: 'src',
+          image: 'xlink:href',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'src',
+          'b-card-img-lazy': ['src', 'blank-src'],
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src'
+        }
+
+        return options
+      })
+  }
 };
