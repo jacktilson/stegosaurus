ba30fd92ca2153280103022747d843a29773cb26
Jack Tweddell <tweddell.jack@gmail.com>
2019-08-14 16:22:06 +0100

    ImageFileViewer component

---
+++ steg_client/src/components/ImageFileViewer.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+    img(:src="imgSrc" :alt="imgFile?imgFile.name:'No Image'")
+</template>
+<script>
+export default {
+    name: "ImageFileViewer",
+    props: {
+        imgFile: {
+            type: File,
+            required: true
+        }
+    },
+    data() { return {
+        reader: new FileReader(),
+        imgSrc = ""
+    }},
+    watch: {
+        imgFile(val){
+            this.reader.readAsDataUrl(val)   
+        }
+    },
+    created(){
+        this.reader.onload = event => {
+            this.imgSrc = event.target.result
+        }
+    }
+}
+</script>
