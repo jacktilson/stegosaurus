0e41669f96f2d6272f1d7ea98278ba219167562a
Jack Tweddell <tweddell.jack@gmail.com>
2019-08-18 17:03:22 +0100

    About Page

--- steg_client/src/views/Error.vue
+++ steg_client/src/views/Error.vue
@@ -7,21 +7,5 @@
                     .card-body
                         h2 This page went extinct by the looks of things...
                         h2 Are you sure you meant to come here?
-                        router-link.btn.my-3(to="/encode") Go Back In Time
-
+                        router-link.btn.my-3.btn-lg.btn-brand(to="/encode") Go Back In Time
 </template>
-<style lang="scss">
-.btn {
-  border: 1px solid rgb(63, 127, 63);
-  background-color: rgb(63, 127, 63);
-  border-radius: 5px;
-  padding: 14px 28px;
-  font-size: 16px;
-  cursor: pointer;
-  &:hover {
-    transition: 256ms;
-    background-color: rgb(31, 95, 31);
-    border-color: rgb(31, 95, 31);
-  }
-}
-</style>
