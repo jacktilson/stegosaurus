1c0b1a7e0ae39f173e81d42ddfb1cd45c822a009
Oliver Butler <olly@protoncloud.co.uk>
2019-08-20 01:39:09 +0100

    Encode validation

--- steg_client/src/components/Navbar.vue
+++ steg_client/src/components/Navbar.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
-    b-navbar(toggleable="lg" sticky)
-        .container
-            b-navbar-brand.my-0.mx-3
-                img(src="@/assets/ct_logo.svg" height="96px")
-            b-navbar-toggle(target="nav-collapse").my-0.mx-0
-                font-awesome-icon(icon="bars" size="2x")
-            b-collapse(id="nav-collapse" is-nav).mx-3
-                b-navbar-nav.row.w-100.text-lg-center
-                    b-nav-item.col-lg(to="/about") About
-                    b-nav-item.col-lg(to="/encode") Encode
-                    b-nav-item.col-lg(to="/decode") Decode
+  b-navbar(toggleable="lg" sticky)
+    .container
+      b-navbar-brand.my-0.mx-3
+        img(src="@/assets/ct_logo.svg" height="96px")
+      b-navbar-toggle(target="nav-collapse").my-0.mx-0
+        font-awesome-icon(icon="bars" size="2x")
+      b-collapse(id="nav-collapse" is-nav).mx-3
+        b-navbar-nav.row.w-100.text-lg-center
+          b-nav-item.col-lg(to="/about") About
+          b-nav-item.col-lg(to="/encode") Encode
+          b-nav-item.col-lg(to="/decode") Decode
 </template>
 
 <script>
