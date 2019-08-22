eb70b493bc55ea44190447c76db6a6cdeb96bf46
Jack T <jacktilson80@gmail.com>
2019-08-23 00:10:41 +0100

    Fix destructive delete get request (finally this time)

--- steg_client/src/views/Encode.vue
+++ steg_client/src/views/Encode.vue
@@ -371,7 +371,7 @@ export default {
       }
     },
     delete() {
-      var is_deleted;
+      var window.is_deleted = false;
       axios
         .get("/encode/delete", {
         params: {
@@ -399,7 +399,7 @@ export default {
             response.headers["content-disposition"]
           ).groups.filename;
           saveAs(new Blob([response.data]), filename);
-          if (is_deleted == false){
+          if (window.is_deleted == false){
             this.delete();
           }
         })
